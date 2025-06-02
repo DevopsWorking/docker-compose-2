03a7296186e52247bb246c2924aacad114542959
Karan Gupta <159310985+DevopsWorking@users.noreply.github.com>
2025-06-02 14:04:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ cache = redis.Redis(host='redis', port=6379)
 def hello():
     count = cache.incr('hits')
     return f'Hello! This page has been viewed {count} times.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
